e7325d6bf06932ebadca74c3adfafd28b2cb3806
Andrea Rescalli <rescalli.are@gmail.com>
2020-11-05 16:45:21 +0100

    Device tested. EEPROM r/w, UART and ISR button work

--- RESCALLI_ANDREA.cydsn/main.c
+++ RESCALLI_ANDREA.cydsn/main.c
@@ -13,6 +13,7 @@
 // Includes
 #include "project.h"
 #include "InterruptRoutines.h"
+#include <stdio.h>
 
 // Defines
 # define STARTUP_REG 0x0000
@@ -29,12 +30,21 @@ static uint8 count_push         = 0; // Flag that informs whether the button has
 static uint8 sampling_frequency = 0; // Varaible that stores the sampling frequency at which the
                                      // accelerometer operates [Hz]   
 
+// TEST
+char msg[50] = {'\0'};
+uint8 test_write_read = 0;
+                                    
+
 int main(void) {
 
     CyGlobalIntEnable; /* Enable global interrupts. */
     
-    // Start components (EEPROM)
+    // Start components (EEPROM, UART)
     EEPROM_Start();
+    UART_Start();
+    
+    // Start ISRs
+    ISR_Push_StartEx(Custom_ISR_Push);    
     
     // Init flags
     flag_push = 0;
@@ -50,27 +60,36 @@ int main(void) {
     
         case A:
             count_push = 1;
+            UART_PutString("1 Hz\r\n");
             break;
         case B:
             count_push = 2;
+            UART_PutString("10 Hz\r\n");
             break;
         case C:
             count_push = 3;
+            UART_PutString("25 Hz\r\n");
             break;
         case D:
             count_push = 4;
+            UART_PutString("50 Hz\r\n");
             break;        
         case E:
             count_push = 5;
+            UART_PutString("100 Hz\r\n");
             break;        
         case F:
             count_push = 6;
+            UART_PutString("200 Hz\r\n");
             break;
         default:
             // Write default value on EEPROM
             EEPROM_UpdateTemperature();
             EEPROM_WriteByte(A,STARTUP_REG);
             
+            test_write_read = EEPROM_ReadByte(STARTUP_REG);
+            sprintf(msg, "Default value set: %d\r\n", test_write_read);
+            UART_PutString(msg);
             sampling_frequency = A;            
             count_push = 1;
             break;
@@ -92,19 +111,32 @@ int main(void) {
             switch(count_push) {
             
                 case 1:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);                    
                     break;
                 case 2:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);
                     break;
                 case 3:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);
                     break;
                 case 4:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);
                     break;
                 case 5:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);
                     break;
                 case 6:
+                    sprintf(msg, "counter: %d\r\n", count_push);
+                    UART_PutString(msg);
                     count_push = 0;
                     break;
                 default:
+                    UART_PutString("Error\r\n");
                     break;
             
             }
